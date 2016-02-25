8da32c2365f6e2e5146d93bb2e43ad74ad4561cc
Idan Adar <idan@adar.me>
2016-02-25 11:31:25 +0200

    add missing var

--- www/js/index.js
+++ www/js/index.js
@@ -14,6 +14,12 @@
 * limitations under the License.
 */
 
+var Messages = {
+    // Add here your messages for the default language.
+    // Generate a similar file with a language suffix containing the translated messages.
+    // key1 : message1,
+};
+
 var wlInitOptions = {
     // Options to initialize with the WL.Client object.
     // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
